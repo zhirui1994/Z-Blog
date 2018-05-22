23302c701845b9422f754d369cdd5b053d4ce48f
zhirui1994 <www.zhirui@foxmail.com>
2018-05-23 00:02:59 +0800

    update

--- src/main/java/com/zhirui/zblog/service/ContentService.java
+++ src/main/java/com/zhirui/zblog/service/ContentService.java
@@ -2,10 +2,14 @@ package com.zhirui.zblog.service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.zhirui.zblog.constant.WebConst;
 import com.zhirui.zblog.dao.ContentVoMapper;
 import com.zhirui.zblog.model.Vo.ContentVo;
 import com.zhirui.zblog.model.Vo.ContentVoExample;
+import com.zhirui.zblog.utils.DateKit;
+import com.zhirui.zblog.utils.TaleUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -16,8 +20,47 @@ public class ContentService {
     @Resource
     private ContentVoMapper contentDao;
 
+    @Resource
+    private MetaService metaService;
+
     public String publish(ContentVo content) {
-        return null;
+        if (content == null) {
+            return "文章对象为空";
+        }
+        if (StringUtils.isEmpty(content.getTitle())) {
+            return "文章标题不能为空";
+        }
+        if (StringUtils.isEmpty(content.getContent())) {
+            return "文章内容不能为空";
+        }
+        int titleLength = content.getTitle().length();
+        if (titleLength > WebConst.MAX_TITLE_COUNT) {
+            return "文章标题过长";
+        }
+        if (!StringUtils.isEmpty(content.getSlug())) {
+            if (content.getSlug().length() < 5) {
+                return "路径太短了";
+            }
+//            if (!TaleUtils.isPath(content.getSlug())) {
+//                return "您输入的路径不合法";
+//            }
+        } else {
+            content.setSlug(null);
+        }
+
+//        content.setContent();
+        int time = DateKit.getCurrentUnixTime();
+        content.setCreated(time);
+        content.setModified(time);
+        content.setHits(0);
+        content.setCommentsNum(0);
+
+        String tags = content.getTags();
+        String categories = content.getCategories();
+        contentDao.insert(content);
+//        Integer cid content.getCid();
+//        metaService.save
+        return WebConst.SUCCESS_RESULT;
     }
 
     public List<ContentVo> getContents(Integer p, Integer limit) {
