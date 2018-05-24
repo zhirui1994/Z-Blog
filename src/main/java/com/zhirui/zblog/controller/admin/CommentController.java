1cb37a002c773e2ea9d1a4f61ff52d7e0c9ef3b7
zhirui1994 <www.zhirui@foxmail.com>
2018-05-24 23:16:06 +0800

    fix

---
+++ src/main/java/com/zhirui/zblog/controller/admin/CommentController.java
@@ -0,0 +1,36 @@
+package com.zhirui.zblog.controller.admin;
+
+import com.github.pagehelper.PageInfo;
+import com.zhirui.zblog.controller.BaseController;
+import com.zhirui.zblog.model.Vo.CommentVo;
+import com.zhirui.zblog.model.Vo.CommentVoExample;
+import com.zhirui.zblog.model.Vo.UserVo;
+import com.zhirui.zblog.service.CommentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping(value = "admin/comments")
+public class CommentController extends BaseController {
+    @Resource
+    private CommentService commentService;
+
+    @GetMapping(value = "")
+    public String index(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "limit", defaultValue = "15") int limit,
+            HttpServletRequest request
+    ) {
+        UserVo user = this.user(request);
+        CommentVoExample commentVoExample = new CommentVoExample();
+        commentVoExample.setOrderByClause("coid desc");
+//        commentVoExample.crea
+        PageInfo<CommentVo> commentVoPageInfo = commentService.getComments(null, 1, 100);
+        return "admin/comment_list";
+    }
+}
